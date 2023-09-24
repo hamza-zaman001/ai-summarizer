dff7d717bbc170a140f6b34e3f1360f32fa1eba6
hamza-zaman001 <hamzabadiuzzamankhan@gmail.com>
2023-09-25 00:52:10 +0500

    idk what to do now ;-;

--- src/services/article.js
+++ src/services/article.js
@@ -1,37 +1,25 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-
-const options = {
-    method: 'GET',
-    url: 'https://article-extractor-and-summarizer.p.rapidapi.com/summarize',
-    params: {
-        url: 'https://time.com/6266679/musk-ai-open-letter/',
-        length: '3'
-    },
-    headers: {
-        'X-RapidAPI-Key': 'b870ca648cmshc224f61290ded2ap1c200fjsn197e4ea3f16c',
-        'X-RapidAPI-Host': 'article-extractor-and-summarizer.p.rapidapi.com'
-    }
-};
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 const rapidApiKey = import.meta.env.VITE_RAPID_API_ARTICLE_KEY;
 
-export const articleApi = createApi ({
+export const articleApi = createApi({
     reducerPath: 'articleApi',
     baseQuery: fetchBaseQuery({
-        baseUrl:'https://article-extractor-and-summarizer.p.rapidapi.com/',
+        baseUrl: 'https://article-extractor-and-summarizer.p.rapidapi.com/',
         prepareHeaders: (headers) => {
-            headers.set('X-RapidAPI-Key', 'rapidApiKey');
+            headers.set('X-RapidAPI-Key', rapidApiKey);
             headers.set('X-RapidAPI-Host', 'article-extractor-and-summarizer.p.rapidapi.com');
 
             return headers;
-        }
+        },
     }),
-
     endpoints: (builder) => ({
         getSummary: builder.query({
-            query: (params) => `/summerize?url=${encodeURIComponent(params.articleUrl)}&length=3`
-        })
-    })
-});
+            // encodeURIComponent() function encodes special characters that may be present in the parameter values
+            // If we do not properly encode these characters, they can be misinterpreted by the server and cause errors or unexpected behavior. Thus that RTK bug
+            query: (params) => `summarize?url=${encodeURIComponent(params.articleUrl)}&length=3`,
+        }),
+    }),
+})
 
-export const { useLazyGetSummaryQuery } = articleApi;
+export const { useLazyGetSummaryQuery } = articleApi
